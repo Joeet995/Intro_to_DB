58a4709b688eda3ac65cce501c3d9d98d123d060
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-26 18:23:59 +0300

    task1

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,12 +3,12 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host = "localhost",
-    user = "yousseftarek95",
-    password = "2597",
+    user = "root",
+    password = "259725987",
     )
 
 mycursor = mydb.cursor()
 
 mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-print("Database 'alx_book_store' created successfully!")
+#print("Database 'alx_book_store' created successfully!")
