0ee61875efe6963c24b0b97664f4fbf9a77952de
Joeet995 <yousef.tarek095@gmail.com>
2024-07-26 13:20:01 +0300

    task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,5 +7,26 @@ CREATE TABLE Books (
 )
 
 CREATE TABLE Authors (
-    
+    author_id PRIMARY KEY
+    author_name VARCHAR(215)
+)
+
+CREATE TABLE Customers(
+    customer_id PRIMARY KEY
+    customer_name VARCHAR(215)
+    email VARCHAR(215
+    address TEXT)
+)
+
+CREATE TABLE Orders(
+    order_id PRIMARY KEY
+    customer_id FOREIGN KEY
+    order_date DATE
+)
+
+CREATE TABLE Order_Details(
+    order_detail_id PRIMARY KEY
+    order_id FOREIGN KEY
+    book_id FOREIGN KEY
+    quantity DOUBLE
 )
