ea5d6d257962c9ae5bb0e34fba5309addd74e582
Joeet995 <yousef.tarek095@gmail.com>
2024-07-26 13:26:16 +0300

    task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,8 +21,8 @@ CREATE TABLE Customers(
 )
 
 CREATE TABLE Orders(
-    order_id PRIMARY KEY
-    customer_id FOREIGN KEY
+    order_id INT PRIMARY KEY
+    customer_id INT FOREIGN KEY REFERENCES Customers(customer_id)
     order_date DATE
 )
 
