91eb0c1859bc6affdbe1eeb304f61a2fc056eb86
youssef Tarek <yousef.tarek095@gmail.com>
2024-07-26 14:08:55 +0300

    task 1

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,9 +1,10 @@
 import mysql.connector
 
+
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "yousseftarek95",
-    passwd = "1234",
+    password = "2597",
     )
 
 mycursor = mydb.cursor()
